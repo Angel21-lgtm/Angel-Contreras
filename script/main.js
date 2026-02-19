4cf62f0925e089e175af71850c026087a426d885
Angel21-lgtm <mc603452@gmail.com>
2026-02-19 09:59:28 -0600

    Subiendo repositorio para ver diseño responsivo

---
+++ script/main.js
@@ -0,0 +1,46 @@
+"use strict";
+
+
+const main = () => {
+    const cursor = document.querySelector("#cursor");
+    const body = document.querySelector("body");
+
+    document.addEventListener("mousemove", (e) => {
+        cursor.style.left = e.clientX + "px";
+        cursor.style.top = e.clientY + "px";
+        body.style.backgroundPositionX = e.pageX + "px";
+        body.style.backgroundPositionY = e.pageY + "px"
+
+        let element = document.createElement("div");
+        element.className = "element";
+        body.prepend(element);
+
+        element.style.left = e.pageX + "px";
+        element.style.top = e.pageY + "px";
+
+        const binartio = () => {
+            const numerosBinarios = ("01").split("");
+            return numerosBinarios[Math.floor(Math.random() * numerosBinarios.length)];
+        }
+
+
+        setTimeout(function() {
+
+            let directionY = Math.random() < 0.5 ? -1 : 1;
+            let directionX = Math.random() < 0.5 ? -1 : 1;
+
+            element.style.top = parseInt(element.style.top) - (directionY * (Math.random() * 200)) + "px";
+            element.style.left = parseInt(element.style.left) - (directionX * (Math.random() * 200)) + "px";
+            element.style.transform = "scale(0.25)";
+            element.innerHTML = binartio();
+            element.style.opacity = 0;
+
+            setTimeout(function() {
+                element.remove();
+            }, 1000);
+
+        }, 10);
+    });
+}
+
+main();
